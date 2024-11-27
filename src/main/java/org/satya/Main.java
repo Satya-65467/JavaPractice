a751e7cc86eb99208503672dcabf3bba8fd84be3
Satya <satyabrat9438@gmail.com>
2024-11-27 13:51:42 +0530

    First commit

---
+++ src/main/java/org/satya/Main.java
@@ -0,0 +1,11 @@
+package org.satya;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=5; i++)
+
+        System.out.println("Hello and welcome!");
+
+        }
+    }
